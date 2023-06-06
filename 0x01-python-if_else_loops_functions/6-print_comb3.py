37270a499dc6852434771bcc18ccf98a69c7a294
horbital1140 <adeemma1993@gmail.com>
2023-06-06 09:26:50 -0700

    print dual number

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+    """
+    The two digits must be different - 01 and 10 are considered identical.
+    """
+for dualnumb1 in range(0, 10):
+    for dualnumb2 in range(dualnumb1 + 1, 10):
+        if dualnumb1 == 8 and dualnumb2 == 9:
+            print("{}{}".format(dualnumb1, dualnumb2))
+        else:
+            print("{}{}".format(dualnumb1, dualnumb2), end=", ")
+
