09a523c4fca4ef2f01c35479c37d62d35defc22a
horbital1140 <adeemma1993@gmail.com>
2023-06-06 08:19:05 -0700

    is negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_numb= abs(number) % 10
+last_numb = abs(number) % 10
 if number < 0:
     last_numb = -last_numb
 print("Last digit of {} is {} and is ".format(number, last_numb), end="")
