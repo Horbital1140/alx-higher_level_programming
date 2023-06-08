dae8315a0b0cf84bc3f24f10a6a1b73dd4a8e04b
horbital1140 <adeemma1993@gmail.com>
2023-06-08 06:37:33 -0700

    sum two integer with function add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __main__ == "__name__":
+    """print the sum_of_a + b"""
+    from add_0 import add
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+i learn import and module by practice
