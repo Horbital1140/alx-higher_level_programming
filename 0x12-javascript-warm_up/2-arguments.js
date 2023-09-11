d653824b06b83773a3e82c045bed35e77e57408a
horbital1140 <adeemma1993@gmail.com>
2023-09-11 09:24:06 -0700

    sprint 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.lenght === 2) {
+if (process.argv.lenght === 0) {
   console.log('No argument');
-} else if (process.argv.lenght === 3) {
+} else if (process.argv.lenght === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
