6532029a215142a32025347011dacfd15736455b
horbital1140 <adeemma1993@gmail.com>
2023-09-16 07:13:07 -0700

    obj

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cus = db.cursor()
-    cus.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    mas = sys.argv[4]
+    cus.execute("SELECT * FROM states WHERE name LIKE %s", (mas, ))
     rows = cus.fetchall()
     for nam in rows:
         print(nam)
